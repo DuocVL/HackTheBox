f88ac53fa835aa2da894fee0878924dc1898c132
DuocVL <leduochhnd@gmail.com>
2026-09-08 20:53:06 +0700

    Finish challenge Exatlon

---
+++ Reversing/Exatlon/decrypt.py
@@ -0,0 +1,11 @@
+encrypt="1152 1344 1056 1968 1728 816 1648 784 1584 816 1728 1520 1840 1664 784 1632 1856 1520 1728 816 1632 1856 1520 784 1760 1840 1824 816 1584 1856 784 1776 1760 528 528 2000 "
+
+list = encrypt.split(" ")
+list.pop()# xóa phần tử '' ở cuối
+print(f"Chuỗi mã hóa: {encrypt}")
+print(f"Chuỗi tách: {list}")
+flag = ""
+for c in list:
+    i = int(c)
+    flag += str(chr(i >> 4))#Dịch phải 4 bit, chuyển thành kí tự ASCII và nối vào chuỗi
+print(f"Flag: {flag}")
